e76ca7e80d0a85c8cf84c31b6121b440a5497b8a
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2024-10-01 07:30:26 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    float cm, r, t, i;
+    cout << "Quanto costa la moto?" << endl;
+    cin >> cm;
+    cout << "Quanti sono i tuoi risparmi?" << endl;
+    cin >> r;
+    i = i +1;
+while (cm>r)
+{
+    cout << "Ti servono ancora " << cm-r << " euro" << endl;
+    cout << "Quanti sondi hai risparmiato oggi?" << endl;
+    cin >> t;
+    r=r+t;
+    i=i+1;
+
+}
+cout << "Sei riuscito a comprare la moto, hai accumulato un totale di " << r << " risparmi per un totale di " << i << " volte. E te ne rimangono " << r-cm << endl;
+
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
